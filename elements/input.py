fcc035e1c1fc479bb04e19bb272cb3226a934535
Vladimir <ushakov.vladimir98@mail>
2026-01-16 19:40:32 +0400

    добавление считывание покрытия

--- elements/input.py
+++ elements/input.py
@@ -2,6 +2,7 @@ import allure
 from playwright.sync_api import expect, Locator
 from utils.logger import get_logger
 from elements.base_element import BaseElement
+from ui_coverage_tool import ActionType
 
 logger = get_logger('Input')
 
@@ -13,6 +14,9 @@ class Input(BaseElement):
     def get_locator(self, nth: int=0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('input')
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(nth, **kwargs)}//input'
+
     def fill(self, value: str, nth: int=0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
@@ -20,9 +24,13 @@ class Input(BaseElement):
             logger.info(step)
             locator.fill(value)
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int=0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
